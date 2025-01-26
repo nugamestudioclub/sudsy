90729ed3f4172bb352015a930ccdfb54b8c24bf8
brassilbatwit <56983102+BBBrassil@users.noreply.github.com>
2025-01-26 07:56:00 -0500

    Added sound effectx

--- src/PlayScene.cs
+++ src/PlayScene.cs
@@ -9,12 +9,15 @@ public class PlayScene : Node2D {
 
 	public Soap Soap { get; private set; }
 
-    private float _timeUntilMoveEmission = 0;
+	public SFX SFX { get; private set; }
 
-    [Export]
-    private float _moveEmissionInterval = 0.15f;
-    [Export]
-    private float _slideEmissionInterval = 0.075f;
+
+	private float _timeUntilMoveEmission = 0;
+
+	[Export]
+	private float _moveEmissionInterval = 0.15f;
+	[Export]
+	private float _slideEmissionInterval = 0.075f;
 
 	private Vector2 _uiOffset;
 
@@ -22,74 +25,74 @@ public class PlayScene : Node2D {
 
 	public UI UI { get; private set; }
 
-    [Export]
-    private float _minCleanSpeed = 5f;
+	[Export]
+	private float _minCleanSpeed = 5f;
 
 	[Export]
 	public float BaseCleanRate { get; private set; } = .05f;
 
-    [Export]
-    private float _moveCleanMod = .5f;
+	[Export]
+	private float _moveCleanMod = .5f;
 
-    public float MoveCleanRate => BaseCleanRate * _moveCleanMod;
+	public float MoveCleanRate => BaseCleanRate * _moveCleanMod;
 
-    [Export]
+	[Export]
 	private float _slideCleanMod = 1;
 
 	public float SlideCleanRate => BaseCleanRate * _slideCleanMod;
 
 	[Export]
-    private float _midairJumpCleanMod = 100;
+	private float _midairJumpCleanMod = 100;
 
-    public float MidairJumpCleanRate => BaseCleanRate * _midairJumpCleanMod;
+	public float MidairJumpCleanRate => BaseCleanRate * _midairJumpCleanMod;
 
 
-    private List<Dirt> _dirts = new List<Dirt>();
+	private List<Dirt> _dirts = new List<Dirt>();
 
 	public float TotalDirtAmount { get; private set; }
 	public float CurrentDirtAmount { get; private set; }
 
-    private List<Door> _doors = new List<Door>();
+	private List<Door> _doors = new List<Door>();
 
 
-    [Export]
-    private NodePath _startingDoor;
+	[Export]
+	private NodePath _startingDoor;
 
 
-    public override void _Ready() {
+	public override void _Ready() {
 		Player = GetNode<Player>("Player");
 		Soap = GetNode<Soap>("Soap");
 		Camera = GetNode<Camera2D>("Camera2D");
 		UI = GetNode<UI>("UI");
+		SFX = GetNode<SFX>("SFX");
 		_uiOffset = UI.GlobalPosition - Camera.GlobalPosition;
-        foreach (var node in GetTree().GetNodesInGroup("Dirt"))
-            if (node is Dirt dirt)
-			{
+		foreach( var node in GetTree().GetNodesInGroup("Dirt") )
+			if( node is Dirt dirt ) {
 				TotalDirtAmount += dirt.Amount;
-                _dirts.Add(dirt);
-            }    
+				_dirts.Add(dirt);
+			}
 		CurrentDirtAmount = TotalDirtAmount;
 		DrawSoapUI(Player);
 		DrawCleanUI();
-        GD.Print($"Starting Dirt {TotalDirtAmount}");
-        foreach (var node in GetTree().GetNodesInGroup("Door"))
-            if (node is Door door)
-                _doors.Add(door);
-        Enter(GetNode<Door>(_startingDoor));
-        
-    }
-	
+		GD.Print($"Starting Dirt {TotalDirtAmount}");
+		foreach( var node in GetTree().GetNodesInGroup("Door") )
+			if( node is Door door )
+				_doors.Add(door);
+		Enter(GetNode<Door>(_startingDoor));
+
+	}
+
 
 	public override void _PhysicsProcess(float delta) {
 		ProcessPlayerState(Player, delta);
 		ProcessPlayerAnimation(Player, delta);
-        foreach (var dirt in _dirts)
-            ProcessDirtInteraction(Player, dirt, delta);
+		foreach( var dirt in _dirts )
+			ProcessDirtInteraction(Player, dirt, delta);
 
-        MovePlayer(Player, delta);
+		MovePlayer(Player, delta);
 		ProcessSoap(Player, delta);
-        
-        foreach ( var door in _doors )
+
+		foreach( var door in _doors )
 			ProcessDoorInteraction(Player, door, delta);
 	}
 
@@ -111,7 +114,8 @@ public class PlayScene : Node2D {
 			);
 	}
 
-    private bool IsJumping(Player player) {
+
+	private bool IsJumping(Player player) {
 		return !player.IsGrounded
 			&& _input.Get(ButtonKind.Jump)
 			&& player.TimeInState <= player.JumpDuration && player.LinearVelocity.y < 0;
@@ -120,13 +124,23 @@ public class PlayScene : Node2D {
 		return player.IsGrounded && !Mathf.IsZeroApprox(player.LinearVelocity.x);
 	}
 
-    private bool IsSliding(Player player)
-    {
-        return player.IsSliding;
+	private bool IsSliding(Player player) {
+		return player.IsSliding;
 
-    }
+	}
 
-    private bool IsIdle(Player player) {
+	private bool IsFailingMidairJump(Player player) {
+		return Input.IsActionJustPressed("jump")
+			&& !player.IsGrounded
+			&& player.JumpCount < 1
+			&& player.Soap < player.MidairJumpSoapCost;
+	}
+
+	private bool IsFailingSlide(Player player) {
+		return Input.IsActionJustPressed("slide") && player.Soap < player.SlideSoapCost;
+	}
+
+	private bool IsIdle(Player player) {
 		return player.IsGrounded && Mathf.IsZeroApprox(player.LinearVelocity.x);
 	}
 
@@ -139,6 +153,10 @@ public class PlayScene : Node2D {
 			&& !player.IsGrounded
 			&& player.JumpCount < 1
 			&& player.Soap >= player.MidairJumpSoapCost;
+	}
+
+	private bool IsStartingSlide(Player player) {
+		return Input.IsActionJustPressed("slide") && player.Soap >= player.SlideSoapCost;
 	}
 
 	private void ProcessDoorInteraction(Player player, Door door, float delta) {
@@ -157,8 +175,7 @@ public class PlayScene : Node2D {
 		}
 	}
 
-	private void ProcessClean(Dirt dirt, float amount)
-	{
+	private void ProcessClean(Dirt dirt, float amount) {
 		float currentAmount = dirt.Amount;
 		float nextAmount = Math.Max(0, currentAmount - amount);
 		float delta = currentAmount - nextAmount;
@@ -170,19 +187,16 @@ public class PlayScene : Node2D {
 		}
 	}
 
-	private void ProcessDirtInteraction(Player player, Dirt dirt, float delta)
-	{
-		if (!dirt.OverlapsArea(player.Brush)) return;
-		if (player.IsJumpingInMidair)
-		{
+	private void ProcessDirtInteraction(Player player, Dirt dirt, float delta) {
+		if( !dirt.OverlapsArea(player.Brush) ) return;
+		if( player.IsJumpingInMidair ) {
 			ProcessClean(dirt, MidairJumpCleanRate);
 		}
-		else if (player.IsGrounded && Math.Abs(Player.LinearVelocity.x) >= _minCleanSpeed)
-		{
+		else if( player.IsGrounded && Math.Abs(Player.LinearVelocity.x) >= _minCleanSpeed ) {
 			ProcessClean(dirt, IsSliding(player) ? SlideCleanRate : MoveCleanRate);
-        }
-		
-		
+		}
+
+
 	}
 
 	private void ProcessInput(float delta) {
@@ -222,86 +236,87 @@ public class PlayScene : Node2D {
 			_input.Reset(ButtonKind.Interact);
 	}
 
-	
-    private void MovePlayer(Player player, float delta) {
+
+	private void MovePlayer(Player player, float delta) {
 		float inputX = _input.X;
- 
-        if ( !Mathf.IsZeroApprox(inputX) ) {
+
+		if( !Mathf.IsZeroApprox(inputX) ) {
 			player.MoveX(inputX, delta);
 			player.Face(inputX);
-        }
+		}
 
 		if( IsStartingJump(player) ) {
 			player.Jump(delta);
 			Soap.Jump(player.Feet.GlobalPosition);
+			SFX.Play("Jump");
 		}
 		else if( IsStartingMidairJump(player) ) {
 			player.MidairJump(delta);
 			player.Soap = Math.Max(player.Soap - player.MidairJumpSoapCost, 0);
 			DrawSoapUI(player);
 			Soap.MidairJump(player.Feet.GlobalPosition);
+			SFX.Play("MidairJump");
 		}
 		else if( !IsJumping(player) ) {
 			_input.Reset(ButtonKind.Jump);
 		}
+		if( IsStartingSlide(player) ) {
+			SFX.Play("Slide");
+		}
+		else if( IsFailingSlide(player) ) {
+			SFX.Play("SoaplessSlide");
+
+		}
+		else if( IsFailingMidairJump(player) ) {
+			SFX.Play("SoaplessMidairJump");
+		}
 	}
 
-    private void ShowMoveParticles(Player player)
-    {
-        float xOffset = player.Feet.Shape is RectangleShape2D rect ? rect.Extents.x : 0;
-        Soap.Move(player.Feet.GlobalPosition, new Vector2(xOffset, 0), player.LinearVelocity);  
-    }
+	private void ShowMoveParticles(Player player) {
+		float xOffset = player.Feet.Shape is RectangleShape2D rect ? rect.Extents.x : 0;
+		Soap.Move(player.Feet.GlobalPosition, new Vector2(xOffset, 0), player.LinearVelocity);
+	}
 
-    private void ShowSlideParticles(Player player)
-    {
-        Soap.Slide(player.Feet.GlobalPosition, player.LinearVelocity);  
-    }
+	private void ShowSlideParticles(Player player) {
+		Soap.Slide(player.Feet.GlobalPosition, player.LinearVelocity);
+	}
 
-	private void RegenerateSoap(Player player)
-	{
-        player.Soap = Math.Min(player.MaxSoap, player.Soap + player.SoapRegenRate);
+	private void RegenerateSoap(Player player) {
+		player.Soap = Math.Min(player.MaxSoap, player.Soap + player.SoapRegenRate);
 		DrawSoapUI(player);
-    }
+	}
 
-    private void ProcessSoap(Player player, float delta)
-	{
-		if (_timeUntilMoveEmission <= 0)
-		{
-            if(IsSliding(player))
-			{
-				if (player.Soap > 0)
-				{
-                    ShowSlideParticles(player);
-                    player.Soap = Math.Max(0, player.Soap - player.SlideSoapCost);
+	private void ProcessSoap(Player player, float delta) {
+		if( _timeUntilMoveEmission <= 0 ) {
+			if( IsSliding(player) ) {
+				if( player.Soap > 0 ) {
+					ShowSlideParticles(player);
+					player.Soap = Math.Max(0, player.Soap - player.SlideSoapCost);
 					DrawSoapUI(player);
 					_timeUntilMoveEmission = _slideEmissionInterval;
-                }
-				else
-				{
-                    ShowMoveParticles(player);
-                    _timeUntilMoveEmission = _moveEmissionInterval;
-                }
+				}
+				else {
+					ShowMoveParticles(player);
+					_timeUntilMoveEmission = _moveEmissionInterval;
+				}
 			}
-			else
-			{
-                if (player.IsGrounded)
-                {
-                    ShowMoveParticles(player);
-                    _timeUntilMoveEmission = _moveEmissionInterval;
+			else {
+				if( player.IsGrounded ) {
+					ShowMoveParticles(player);
+					_timeUntilMoveEmission = _moveEmissionInterval;
 
-                }
-            }
+				}
+			}
 
-        }
+		}
 		else {
-            _timeUntilMoveEmission = Math.Max(_timeUntilMoveEmission - delta, 0);
-        }
-		if (!IsSliding(player))
-		{
+			_timeUntilMoveEmission = Math.Max(_timeUntilMoveEmission - delta, 0);
+		}
+		if( !IsSliding(player) ) {
 			RegenerateSoap(player);
 		}
-        
-    }
+
+	}
 
 	private void ProcessPlayerAnimation(Player player, float delta) {
 		player.PlayAnimation(player.GetDesiredAnimation());
---
+++ src/SFX.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+using System.Collections.Generic;
+
+public class SFX : Node {
+	private Dictionary<string, AudioStreamPlayer> _audioStreamPlayers = new Dictionary<string, AudioStreamPlayer>();
+
+	public override void _Ready() {
+		AddAudioStreamPlayer("Jump");
+		AddAudioStreamPlayer("MidairJump");
+		AddAudioStreamPlayer("SoaplessMidairJump");
+		AddAudioStreamPlayer("Slide");
+		AddAudioStreamPlayer("SoaplessSlide");
+		AddAudioStreamPlayer("CollectSoap");
+	}
+
+	public void Play(string name) {
+		var audioStreamPlayer = _audioStreamPlayers[name];
+		audioStreamPlayer.Play(0f);
+	}
+
+	private void AddAudioStreamPlayer(string path) {
+		_audioStreamPlayers[path] = GetNode<AudioStreamPlayer>(path);
+	}
+}
